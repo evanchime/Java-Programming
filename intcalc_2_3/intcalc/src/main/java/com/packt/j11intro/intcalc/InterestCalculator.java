e2b59acef3765960f944a1a4a0a95e9c7a1f2830
Evan Chime <chime.evan.dri.devel@gmail.com>
2020-02-03 17:03:47 +0000

    First version of intcalc

---
+++ intcalc_2_3/intcalc/src/main/java/com/packt/j11intro/intcalc/InterestCalculator.java
@@ -0,0 +1,13 @@
+package main.java.com.packt.j11intro.intcalc;
+
+public class InterestCalculator {
+    public InterestCalculator(){ // constructor
+
+    }
+
+    public int calcInterest(int owedInCents, int ratePerc) {
+        return 0;
+    }
+
+
+}
