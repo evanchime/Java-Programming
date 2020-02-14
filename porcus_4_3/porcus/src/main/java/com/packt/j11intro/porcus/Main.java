e9184595fc064ded3c0042e427e66fbaa7b8c403
Evan Chime <chime.evan.dri.devel@gmail.com>
2020-02-14 16:07:19 +0000

    First version of porcus_4_3/porcus

---
+++ porcus_4_3/porcus/src/main/java/com/packt/j11intro/porcus/Main.java
@@ -0,0 +1,15 @@
+package com.packt.j11intro.porcus;
+
+import java.io.IOException;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Initializes main engine.
+        PigLatinTranslator t = new PigLatinTranslator();
+
+        String sentence = "These are the words we want to experiment with for now.";
+        System.out.println("The pig latin for [" + sentence + "] is [" + t.convert(sentence) + "].");
+    }
+}
+
