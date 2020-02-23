5b2d643b4f50497ae8c894c437ac793df2bc7790
Evan Chime <chime.evan.dri.devel@gmail.com>
2020-02-23 16:47:12 +0000

    Add Java-Programming untracked files manually

--- slackbot_3_1/slackbot/src/main/java/com/packt/j11intro/slackbot/Application.java
+++ slackbot_3_1/slackbot/src/main/java/com/packt/j11intro/slackbot/Application.java
@@ -1,4 +1,13 @@
 package main.java.com.packt.j11intro.slackbot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(SlackBotProperties.class)
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
